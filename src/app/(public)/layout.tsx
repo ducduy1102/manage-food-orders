08b541278c7a501cff9ea75b841c3113a99894bb
ducduy1102 <ducduy1110u@gmail.com>
2024-06-19 20:41:09 +0700

    UI Login

---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,60 @@
+import Link from "next/link";
+import { Menu, Package2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import DarkModeToggle from "@/components/dark-mode-toggle";
+import NavItems from "@/app/(public)/nav-items";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="relative flex flex-col w-full min-h-screen">
+      <header className="sticky top-0 flex items-center h-16 gap-4 px-4 border-b bg-background md:px-6">
+        <nav className="flex-col hidden gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+          <Link
+            href="#"
+            className="flex items-center gap-2 text-lg font-semibold md:text-base"
+          >
+            <Package2 className="w-6 h-6" />
+            <span className="sr-only">Big boy</span>
+          </Link>
+          <NavItems className="flex-shrink-0 transition-colors text-muted-foreground hover:text-foreground" />
+        </nav>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="shrink-0 md:hidden"
+            >
+              <Menu className="w-5 h-5" />
+              <span className="sr-only">Toggle navigation menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left">
+            <nav className="grid gap-6 text-lg font-medium">
+              <Link
+                href="#"
+                className="flex items-center gap-2 text-lg font-semibold"
+              >
+                <Package2 className="w-6 h-6" />
+                <span className="sr-only">Big boy</span>
+              </Link>
+
+              <NavItems className="transition-colors text-muted-foreground hover:text-foreground" />
+            </nav>
+          </SheetContent>
+        </Sheet>
+        <div className="ml-auto">
+          <DarkModeToggle />
+        </div>
+      </header>
+      <main className="flex flex-col flex-1 gap-4 p-4 md:gap-8 md:p-8">
+        {children}
+      </main>
+    </div>
+  );
+}
