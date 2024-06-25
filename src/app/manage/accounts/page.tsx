191c51a823cd4b25fcd455c1eed10e17bd29db1c
ducduy1102 <ducduy1110u@gmail.com>
2024-06-25 11:25:43 +0700

    Fix bug useSearchParam khi build và bug avatar ở trang setting

--- src/app/manage/accounts/page.tsx
+++ src/app/manage/accounts/page.tsx
@@ -1,21 +1,30 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-import AccountTable from '@/app/manage/accounts/account-table'
+import AccountTable from "@/app/manage/accounts/account-table";
+import { Suspense } from "react";
 
-export default function Dashboard() {
+export default function AccountsPage() {
   return (
-    <main className='grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8'>
-      <div className='space-y-2'>
-        <Card x-chunk='dashboard-06-chunk-0'>
+    <main className="grid items-start flex-1 gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+      <div className="space-y-2">
+        <Card x-chunk="dashboard-06-chunk-0">
           <CardHeader>
             <CardTitle>Tài khoản</CardTitle>
             <CardDescription>Quản lý tài khoản nhân viên</CardDescription>
           </CardHeader>
           <CardContent>
-            <AccountTable />
+            <Suspense>
+              <AccountTable />
+            </Suspense>
           </CardContent>
         </Card>
       </div>
     </main>
-  )
+  );
 }
