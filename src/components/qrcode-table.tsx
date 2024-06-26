ea684619724c2e99f4c066bd617e3f1d9ae917af
ducduy1102 <ducduy1110u@gmail.com>
2024-06-26 19:59:58 +0700

    Adding information to QR Code canvas

--- src/components/qrcode-table.tsx
+++ src/components/qrcode-table.tsx
@@ -15,16 +15,41 @@ export default function QRCodeTable({
 }) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   useEffect(() => {
-    const canvas = canvasRef.current;
+    // Canvas thật
+    const canvas = canvasRef.current!;
+    canvas.height = width + 70;
+    canvas.width = width;
+    const canvasContext = canvas.getContext("2d")!;
+    canvasContext.fillStyle = "#fff";
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+    canvasContext.font = "20px Arial";
+    canvasContext.textAlign = "center";
+    canvasContext.fillStyle = "#000";
+    canvasContext.fillText(
+      `Bàn số ${tableNumber}`,
+      canvas.width / 2,
+      canvas.width + 20
+    );
+    canvasContext.fillText(
+      `Quét mã QR để gọi món`,
+      canvas.width / 2,
+      canvas.width + 50
+    );
+    const virtualCanvas = document.createElement("canvas");
     QRCode.toCanvas(
-      canvas,
+      virtualCanvas,
       getTableLink({
         token,
         tableNumber,
       }),
+      {
+        width,
+        margin: 4,
+      },
       function (error) {
         if (error) console.error(error);
-        console.log("success!");
+        // console.log("success!");
+        canvasContext.drawImage(virtualCanvas, 0, 0, width, width);
       }
     );
   }, [token, tableNumber]);
