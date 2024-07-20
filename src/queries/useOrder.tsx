639f32cc150ec77825bcd0c5c47ed0aec4cae9ff
ducduy1102 <ducduy1110u@gmail.com>
2024-07-20 20:41:32 +0700

    Declare order APIs and display data on the UI

--- src/queries/useOrder.tsx
+++ src/queries/useOrder.tsx
@@ -1,5 +1,8 @@
 import orderApiRequest from "@/apiRequests/order";
-import { UpdateOrderBodyType } from "@/schemaValidations/order.schema";
+import {
+  GetOrdersQueryParamsType,
+  UpdateOrderBodyType,
+} from "@/schemaValidations/order.schema";
 import { useMutation, useQuery } from "@tanstack/react-query";
 
 export const useUpdateOrderMutation = () => {
@@ -12,9 +15,23 @@ export const useUpdateOrderMutation = () => {
   });
 };
 
-export const useGetOrderList = () => {
+export const useGetOrderList = (queryParams: GetOrdersQueryParamsType) => {
   return useQuery({
-    queryFn: orderApiRequest.getOrderList,
-    queryKey: ["admin-orders"],
+    queryFn: () => orderApiRequest.getOrderList(queryParams),
+    queryKey: ["admin-orders", queryParams],
+  });
+};
+
+export const useGetQueryDetail = ({
+  id,
+  enabled,
+}: {
+  id: number;
+  enabled: boolean;
+}) => {
+  return useQuery({
+    queryFn: () => orderApiRequest.getOrderDetail(id),
+    queryKey: ["orders", id],
+    enabled,
   });
 };
