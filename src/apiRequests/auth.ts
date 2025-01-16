6c1ed08e46c3d0747bc10dda89f8e9e954a6b67d
ducduy1102 <ducduy1110u@gmail.com>
2025-01-16 23:15:26 +0700

    Integrate google login for next.js

--- src/apiRequests/auth.ts
+++ src/apiRequests/auth.ts
@@ -45,6 +45,8 @@ const authApiRequest = {
     this.refreshTokenRequest = null;
     return result;
   },
+  setTokenToCookie: (body: { accessToken: string; refreshToken: string }) =>
+    http.post("/api/auth/token", body, { baseUrl: "" }),
 };
 
 export default authApiRequest;
