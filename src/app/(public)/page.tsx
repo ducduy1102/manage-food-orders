6941c577ea337ffa773a074190ffc1b7873baa08
ducduy1102 <ducduy1110u@gmail.com>
2025-01-17 14:21:03 +0700

    i18n integration without routing

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,10 +1,12 @@
 import dishApiRequest from "@/apiRequests/dish";
 import { formatCurrency } from "@/lib/utils";
 import { DishListResType } from "@/schemaValidations/dish.schema";
+import { getTranslations } from "next-intl/server";
 import Image from "next/image";
 import Link from "next/link";
 
 export default async function Home() {
+  const t = await getTranslations("HomePage");
   let dishList: DishListResType["data"] = [];
   try {
     const result = await dishApiRequest.list();
@@ -29,7 +31,7 @@ export default async function Home() {
         />
         <div className='relative z-20 px-4 py-10 md:py-20 sm:px-10 md:px-20'>
           <h1 className='text-xl font-bold text-center sm:text-2xl md:text-4xl lg:text-5xl'>
-            Nhà hàng Big Boy
+            {t("title")}
           </h1>
           <p className='mt-4 text-sm text-center sm:text-base'>
             Vị ngon, trọn khoảnh khắc
