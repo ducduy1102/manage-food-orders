e0d8a40404ec8da67925bf0e07fce85e691db04a
ducduy1102 <ducduy1110u@gmail.com>
2025-01-18 13:20:23 +0700

    SEO meta data i18n

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,17 +6,35 @@ import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/theme-provider";
 import AppProvider from "@/components/app-provider";
 import { NextIntlClientProvider } from "next-intl";
-import { getMessages, setRequestLocale } from "next-intl/server";
+import {
+  getMessages,
+  getTranslations,
+  setRequestLocale,
+} from "next-intl/server";
 import { routing } from "@/i18n/routing";
+import { Locale } from "@/config";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
-export const metadata: Metadata = {
-  title: "HaNa Restaurant",
-  description: "The best restaurant in the world",
-};
+
+// export const metadata: Metadata = {
+//   title: "HaNa Restaurant",
+//   description: "The best restaurant in the world",
+// };
+
+export async function generateMetadata({
+  params: { locale },
+}: {
+  params: { locale: Locale };
+}) {
+  const t = await getTranslations({ locale, namespace: "Brand" });
+  return {
+    title: t("title"),
+    description: "The best restaurant in the world",
+  };
+}
 
 export function generateStaticParams() {
   return routing.locales.map((locale) => ({ locale }));
